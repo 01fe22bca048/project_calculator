4a1bdec6d0a47d8bd06bd38e61b846ed7b0047e5
01fe22bca048 <01fe22bca048@kletech.ac.in>
2024-02-09 09:54:06 +0530

    Performing inbuilt math functions in java

---
+++ mathoperation.java
@@ -0,0 +1,24 @@
+ public class mathoperation {
+    public double squareRoot(double a) {
+        return Math.sqrt(a);
+    }
+
+    public double power(double a, double b) {
+        return Math.pow(a, b);
+    }
+
+    public double minimum(double a,double b) {
+        return Math.min(a,b);
+    }
+
+    public double maximum(double a , double b) {
+        return Math.max(a,b);
+    }
+
+    public double absolute(double a) {
+        return Math.abs(a);
+    }
+
+{
+}
+}

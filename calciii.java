0ccdf0dfb70f541669915a4724174a0ed904fce6
01fe22bca048 <156497675+01fe22bca048@users.noreply.github.com>
2024-02-19 14:30:21 +0530

    Update calciii.java

--- calciii.java
+++ calciii.java
@@ -2,22 +2,21 @@ import java.io.*;
  
 // Driver Class
 class calciii {
-    public  float sum(float num1, float num2)
+    public float sum(float num1, float num2)
     {
-        return num1+num2;
+        return num1 + num2;
     }
 
-    public  float sub(float num1, float num2)
+    public float sub(float num1, float num2)
     {
-        return num1-num2;
+        return num1 - num2;
     }
      
-     
-      // main function
+    // main function
     public static void main(String[] args)
     {
         float num1 = 10, num2 = 20, res = 0;
-        addition ob = new addition();
+        calciii ob = new calciii();
         res = ob.sum(num1, num2);
         System.out.println(res);
 
